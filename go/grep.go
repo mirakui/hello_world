7248f61c652f3a71b26dbbe827818db4e5ec1531
Issei Naruta <naruta@cookpad.com>
2013-09-16 17:30:00 +0900

    Add grep.go

---
+++ go/grep.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func grep(pattern string, infile *os.File) {
+	scanner := bufio.NewScanner(infile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		matched, err := regexp.MatchString(pattern, line)
+		if err != nil {
+			panic(err)
+		}
+		if matched {
+			fmt.Println(line)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+func main() {
+	var pattern string
+	var infile *os.File
+	var err error
+
+	switch count := len(os.Args); {
+	case count > 2:
+		pattern = os.Args[1]
+		infile, err = os.Open(os.Args[2])
+		if err != nil {
+			panic(err)
+		}
+	case count > 1:
+		infile = os.Stdin
+	default:
+		fmt.Println("usage: grep pattern [file]")
+	}
+
+	grep(pattern, infile)
+}
