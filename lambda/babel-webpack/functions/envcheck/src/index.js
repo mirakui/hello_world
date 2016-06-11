bcdaa273163f13c818f0f999aa375c090b65ba86
Issei Naruta <naruta@cookpad.com>
2016-06-11 19:27:33 +0900

    implement envcheck

---
+++ lambda/babel-webpack/functions/envcheck/src/index.js
@@ -0,0 +1,20 @@
+import λ from 'apex.js'
+import 'babel-polyfill'
+import { exec } from 'child_process'
+
+function sh(cmd) {
+  return new Promise( (resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          reject({error: error})
+        }
+        else {
+          resolve({stdout: stdout, stderr: stderr})
+        }
+      })
+  })
+}
+
+export default λ(e => {
+  return sh(e.cmd)
+})
