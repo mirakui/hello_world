0d1a69b4c73b10fbc1ae2f87cafa1a70d4f94cb2
Issei Naruta <naruta@cookpad.com>
2016-06-12 02:25:43 +0900

    example for resizing from s3

--- node_es6/src/img.js
+++ node_es6/src/img.js
@@ -1,13 +1,16 @@
 import 'babel-polyfill'
 import AWS from 'aws-sdk'
+import _gm from 'gm'
 
-async function main() {
-  let s3 = new AWS.S3()
-  let params = {
-    Bucket: 'mirakui-img',
-    Key: 'test/1/1.jpg'
+const gm = _gm.subClass({ imageMagick: true, appPath: '/usr/local/bin/' })
+
+function s3Get(bucket, key) {
+  const s3 = new AWS.S3()
+  const params = {
+    Bucket: bucket,
+    Key: key
   }
-  let data = await new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     s3.getObject(params, (err, data) => {
       if (err) {
         reject(err)
@@ -17,10 +20,28 @@ async function main() {
       }
     })
   })
-  console.log('data.length = ' + data.length)
-  return data
 }
 
-console.log('start')
+function resize(data, width, height, outpath) {
+  return new Promise((resolve, reject) => {
+    gm(data).
+      resize(width, height).
+      write(outpath, (err) => {
+        if (err) {
+          reject(err)
+        }
+        else {
+          resolve(outpath)
+        }
+      })
+  })
+}
+
+async function main() {
+  const resp = await s3Get('mirakui-img', 'test/1/1.jpg')
+  const out = await resize(resp.Body, 100, 100, 'out.jpg')
+  return out
+}
+
 main().then( x => console.log(x) ).catch( e => console.error(e) )
 
