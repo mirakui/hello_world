44b51a4ba08749e6666e74d31d9eb41a0cae546e
Issei Naruta <naruta@cookpad.com>
2016-06-12 10:41:17 +0900

    Update img.js

--- node_es6/src/img.js
+++ node_es6/src/img.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill'
 import AWS from 'aws-sdk'
 import _gm from 'gm'
-
 const gm = _gm.subClass({ imageMagick: true, appPath: '/usr/local/bin/' })
 
 function s3Get(bucket, key) {
@@ -12,6 +11,25 @@ function s3Get(bucket, key) {
   }
   return new Promise((resolve, reject) => {
     s3.getObject(params, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+function s3Put(bucket, key, data) {
+  const s3 = new AWS.S3()
+  const params = {
+    Bucket: bucket,
+    Key: key,
+    Body: data
+  }
+  return new Promise((resolve, reject) => {
+    s3.putObject(params, (err, data) => {
       if (err) {
         reject(err)
       }
@@ -37,42 +55,18 @@ function resize(data, width, height, outpath) {
   })
 }
 
-function s3Put(bucket, key, data) {
-  const s3 = new AWS.S3()
-  const params = {
-    Bucket: bucket,
-    Key: key,
-    Body: data
-  }
-  return new Promise((resolve, reject) => {
-    s3.putObject(params, (err, data) => {
-      if (err) {
-        reject(err)
-      }
-      else {
-        resolve(data)
-      }
-    })
-  })
-}
-
-function main2() {
+function main() {
   const key = 'test/1/1.jpg'
-  return s3Get('mirakui-img', key).then(
+  s3Get('mirakui-img', key).then(
     resp => resize(resp.Body, 100, 100, 'out.jpg')
   ).then(
     buffer => s3Put('mirakui-img-deliver', key, buffer)
   ).catch(
     e => console.error(e)
   )
+  return { key: key }
 }
 
-async function main() {
-  const resp = await s3Get('mirakui-img', 'test/1/1.jpg')
-  const out = await resize(resp.Body, 100, 100, 'out.jpg')
-  return out
-}
-
-main2()
+main()
 // main().then( x => console.log(x) ).catch( e => console.error(e) )
 
