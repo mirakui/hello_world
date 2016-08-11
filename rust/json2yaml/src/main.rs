f311969c1832a0bddde8c4b5f34efef372ad4126
Issei Naruta <naruta@cookpad.com>
2016-08-11 23:29:40 +0900

    Update

--- rust/json2yaml/src/main.rs
+++ rust/json2yaml/src/main.rs
@@ -1,3 +1,33 @@
+// use std::io;
+// use std::fmt;
+
+struct ObjectNode<'a> {
+    key: String,
+    value: &'a ValueNode,
+}
+
+trait ValueNode {
+    fn to_json(&self) -> String;
+}
+
+impl<'a> ValueNode for ObjectNode<'a> {
+    fn to_json(&self) -> String {
+        format!("{{\"{}\": {}}}", self.key, self.value.to_json())
+    }
+}
+impl<'a> ValueNode for String {
+    fn to_json(&self) -> String {
+        format!("\"{}\"", self)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let root: ObjectNode = ObjectNode {
+        key: "foo".to_string(),
+        value: &"bar".to_string(),
+    };
+    let obj: &ValueNode = &root;
+
+    println!("{}", root.to_json());
+    println!("{}", obj.to_json());
 }
